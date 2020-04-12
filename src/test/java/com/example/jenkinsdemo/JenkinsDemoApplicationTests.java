d802792b41adb07fc6c148e6cbf5e2152ffe5bc0
unknown <lukasz.jalowiecki@gmail.com>
2020-04-12 17:28:33 +0200

    initial commit

---
+++ src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
